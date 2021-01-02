4e85008e66d6e5545e31ba5d9fad99e8218e3fd3
heidariarash <arashheidari94@outlook.com>
2021-01-02 17:00:52 +0330

    icon set

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from mainPageUi      import MainPageUi
+from mainPageUi      import Ui_NN2GUI
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, qApp, QDockWidget, QFileDialog
-from PyQt5           import QtWidgets
+from PyQt5           import QtWidgets, QtGui
 
 import sys
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent=parent)
-        self.ui = MainPageUi()
+        self.ui = Ui_NN2GUI()
         self.ui.setupUi(self)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon('gallery/icon.jpg'))
     w = MainWindow()
     w.show()
     sys.exit(app.exec_())
