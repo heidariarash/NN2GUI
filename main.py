e155d5ec4b309fc354d1b1f78ab73a9a00f02c3e
heidariarash <arashheidari94@outlook.com>
2021-01-03 17:45:49 +0330

    qcomboboxes have the correct style now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mainPageUi      import Ui_NN2GUI
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, qApp, QDockWidget, QFileDialog
-from PyQt5           import QtWidgets, QtGui
+from PyQt5           import QtWidgets, QtGui, QtCore
 
 import sys
 
@@ -9,6 +9,9 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(parent=parent)
         self.ui = Ui_NN2GUI()
         self.ui.setupUi(self)
+        QtCore.QTimer.singleShot(100, lambda: self.ui.input_type.setStyleSheet(self.ui.input_type.styleSheet()))
+        QtCore.QTimer.singleShot(100, lambda: self.ui.framework_type.setStyleSheet(self.ui.framework_type.styleSheet()))
+        QtCore.QTimer.singleShot(100, lambda: self.ui.output_type.setStyleSheet(self.ui.output_type.styleSheet()))
         self.ui.predictions_info.setHidden(True)
         self.ui.predictions.setHidden(True)
         self.ui.input_input.setHidden(True)
