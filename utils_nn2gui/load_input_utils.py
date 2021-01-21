0ca6b471855b411b41c87070a289b1a66fb5632a
heidariarash <arashheidari94@outlook.com>
2021-01-21 15:45:46 +0330

    load input utils complete

--- utils_nn2gui/load_input_utils.py
+++ utils_nn2gui/load_input_utils.py
@@ -1,27 +1,22 @@
-from PIL             import Image
 from PyQt5.QtWidgets import QFileDialog
 
-import torch
-# import tensorflow as tf
-
-def get_file(input_type, framework, valid_prep, prep):
+def get_file(input_type):
     if input_type == "Image":
         options     = QFileDialog.Options()
-        fileName, _ = QFileDialog.getOpenFileName(self,"Choose The Model","","Image (*.jpg *.png);;All Files (*)", options=options)
-        if not fileName:
-            return
-
-        if valid_prep:
-            return prep(fileName)
-        returned_file = torch.Image.open(fileName)
-
-
+        fileName, _ = QFileDialog.getOpenFileName(self,"Choose The Image","","Image (*.jpg *.png);;All Files (*)", options=options)
+        return fileName
 
     elif input_type == "Tabular":
-        pass
+        options     = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self,"Choose The Data","","Tabular (*.txt *.csv);;All Files (*)", options=options)
+        return fileName
+
     elif input_type == "Text":
-        pass
-    elif input_type == "Video":
-        pass
-    elif input_type == "3D Image":
-        pass
+        options     = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self,"Choose The Text","","Text (*.txt);;All Files (*)", options=options)
+        return fileName
+
+    elif input_type == "Other":
+        options     = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self,"Choose The Data","","Data (*)", options=options)
+        return fileName
